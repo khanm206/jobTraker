2de0086900aebca73ec163b31c644189224f0397
Moin Khan <moinkhan1119rja@gmail.com>
2026-02-23 17:49:27 +0000

    final test done || 23 February 2026 05:49 PM

--- machine.js
+++ machine.js
@@ -63,7 +63,7 @@ function selectedJob(id, jobs) {
    </div>
 <br class="hidden">
  <button class="del btn border border-neutral/20 p-2 rounded-full">
- <i class="hidden del fa-solid fa-trash-can"></i>
+ <i class="del fa-solid fa-trash-can"></i>
   </button>
 </div>
     `;
